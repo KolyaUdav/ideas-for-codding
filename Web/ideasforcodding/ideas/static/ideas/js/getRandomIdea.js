cc67ed853d2716023b56b3168db944d1750d17e3
KolyaUdav <sashamarunenko@gmail.com>
2019-01-22 17:43:35 +0300

    В getRandomIdea.js реализован таймер, только по окончанию которого может подгружаться и отображаться новая идея.

--- Web/ideasforcodding/ideas/static/ideas/js/getRandomIdea.js
+++ Web/ideasforcodding/ideas/static/ideas/js/getRandomIdea.js
@@ -1,4 +1,18 @@
+var allowed = true;
+
 function getRandomIdea() {
+    if (allowed) {
+        loadIdeaData();
+        allowed = false;
+        allowTimeOut()
+    }
+}
+
+function allowTimeOut() {
+    setTimeout(function() { allowed = true; }, 10000);
+}
+
+function loadIdeaData() {
     $.ajax({
         url: 'random/',
         type: 'get',
